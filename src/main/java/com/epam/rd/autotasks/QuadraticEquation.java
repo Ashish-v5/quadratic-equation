40151c035fe0d5f17ba6be4307056130dcaca61a
thejerome <thejerome@mail.ru>
2021-02-08 15:24:33 +0300

    Initial Commit

---
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -0,0 +1,18 @@
+package com.epam.rd.autotasks;
+
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+
+
+    }
+
+}
