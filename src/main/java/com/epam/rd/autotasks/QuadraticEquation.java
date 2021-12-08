e7d5791fcc3b6995b8930baddb81aaf433b879f7
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2021-12-08 11:27:36 +0300

    Locale fix

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -1,12 +1,13 @@
 package com.epam.rd.autotasks;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 import static java.lang.Math.sqrt;
 
 public class QuadraticEquation {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
