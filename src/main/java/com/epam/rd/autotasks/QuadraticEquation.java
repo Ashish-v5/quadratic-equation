ea0f86f454c39d426b1defeb141ee28780a52457
Ashish7429 <ashish.11916604@lpu.in>
2023-01-22 10:49:27 +0530

    done

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -3,7 +3,7 @@ package com.epam.rd.autotasks;
 import java.util.Locale;
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
+//import static java.lang.Math.sqrt;
 
 public class QuadraticEquation {
     public static void main(String[] args) {
@@ -11,9 +11,18 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
+        double d = b * b - 4.0 * a * c;
+        if (d > 0.0) {
+            double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
+            double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
+            System.out.println(r2 + " " + r1);
+        } else if (d == 0.0) {
+            double r1 = -b / (2.0 * a);
+            System.out.println((int)r1);
+        } else {
+            System.out.println("no roots");
 
-
-
+        }
     }
 
 }
